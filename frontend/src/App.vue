f7d52cb2a0176c6bd305067e552be9d5879240f4
viniciusteixeiradias <viniciustdias2001@gmail.com>
2023-11-26 21:41:02 +1000

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import axios from "axios";
+
+const resp = ref<{ data: string }>({ data: "" });
+
+const fetch = async () => {
+  resp.value = await axios.get("http://localhost:3000");
+}
+</script>
+
+<template>
+  <div>
+    Response: {{ JSON.stringify(resp.data) }}
+  </div>
+  <button @click="fetch">Fetch</button>
+</template>
+
+<style scoped></style>
