48282dfc4d6f3716e6a89f4c7ebc1553f719e7b8
viniciusteixeiradias <viniciustdias2001@gmail.com>
2023-11-26 22:46:50 +1000

    chore: add vue-auth0

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,26 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-import axios from "axios";
+<script lang="ts" setup>
+import { useAuth0 } from '@auth0/auth0-vue';
 
-const resp = ref<{ data: string }>({ data: "" });
+const { loginWithRedirect, logout: authLogout, user, isAuthenticated } = useAuth0();
 
-const fetch = async () => {
-  resp.value = await axios.get("http://localhost:3000");
+const login = () => {
+  loginWithRedirect();
+}
+
+const logout = () => {
+  authLogout({ logoutParams: { returnTo: window.location.origin } });
 }
 </script>
 
 <template>
   <div>
-    Response: {{ JSON.stringify(resp.data) }}
+    <button @click="login">Log in</button>
+    <button @click="logout">Log out</button>
   </div>
-  <button @click="fetch">Fetch</button>
+  <div>
+    <h2>User Profile</h2>
+    <pre v-if="isAuthenticated">
+        <code>{{ user }}</code>
+      </pre>
+  </div>
 </template>
-
-<style scoped></style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createAuth0 } from '@auth0/auth0-vue';
+
+const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+    authorizationParams: {
+      redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL
+    }
+  })
+);
+
+app.mount('#app');
